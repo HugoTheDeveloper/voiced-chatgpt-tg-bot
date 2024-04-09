d4cafec105ef5aac46cd3e53b87a7848d0dbca13
bogdan <telioko@tutanota.com>
2024-04-09 12:38:42 +0300

    fix chat_gpt connection

--- bot/tools/chat_gpt.py
+++ bot/tools/chat_gpt.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'sk-eojihWMYuwlwO4oNjNMX8DbkkkBtLg7I')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 client = OpenAI(
     api_key=OPENAI_API_KEY,
